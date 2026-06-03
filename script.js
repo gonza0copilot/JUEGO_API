02ade9cbbe4d418b7a2a0e3e2911e4bb8d45033f
santiago gonzalez <segonzalez@obralapiedad.com.ar>
2026-06-03 19:12:09 -0300

    commit"me canse de laburar"

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 const ruleta = document.getElementById('ruleta');
 const botonGirar = document.getElementById('botonGirar');
-
-
-
+const preguntaContainer = document.getElementById("pregunta-container");
 let gradosTotales = 0;
+let preguntas = {};
+let respuestaCorrecta = "";
+
+fetch("data.json")
+    .then(respuesta => respuesta.json())
+    .then(datos => {
+        preguntas = datos;
+        console.log("Preguntas cargadas");
+    });
+
+function obtenerPreguntaAleatoria(categoria) {
+
+    const listaPreguntas = preguntas[categoria];
+
+    const indiceAleatorio =
+        Math.floor(Math.random() * listaPreguntas.length);
+
+    return listaPreguntas[indiceAleatorio];
+}
 
 
 
@@ -15,7 +32,16 @@ const categorias = [
     "EXTRA API",
 ];
 
+const mapaCategorias = {
+    "GTA IV": "gta4",
+    "TEAM Fortress 2": "tf2",
+    "LEFT 4 DEAD 2": "l4d2",
+    "MAFIA III": "mafia3"
+};
+
 botonGirar.addEventListener('click', () => {
+
+
 
     const giroAleatorio =
         Math.floor(Math.random() * 3000) + 3000;
@@ -24,7 +50,14 @@ botonGirar.addEventListener('click', () => {
 
     ruleta.style.transform =
         `rotate(${gradosTotales}deg)`;
+
+
 });
+
+
+
+
+
 
 ruleta.addEventListener('transitionend', () => {
 
@@ -34,14 +67,54 @@ ruleta.addEventListener('transitionend', () => {
 
     const indice =
         Math.floor(
-            (((360 - gradosFinales)+83) % 360)
+            (((360 - gradosFinales) + 83) % 360)
             / gradosPorSector
         );
 
     const categoria = categorias[indice];
+    console.log("Categoria detectada:", categoria);
+    console.log("Clave JSON:", mapaCategorias[categoria]);
 
     console.log("Categoría:", categoria);
     alert("Salió: " + categoria);
 
+
+
+
+    if (categoria !== "EXTRA API") {
+
+        const pregunta =
+            obtenerPreguntaAleatoria(
+                mapaCategorias[categoria]
+            );
+
+        console.log(pregunta);
+
+        preguntaContainer.innerHTML = `
+    <h2>${pregunta.pregunta}</h2>
+
+    <button class="opcioness">${pregunta.opciones[0]}</button>
+    <button class="opcioness">${pregunta.opciones[1]}</button>
+    <button class="opcioness">${pregunta.opciones[2]}</button>
+    <button class="opcioness">${pregunta.opciones[3]}</button>
+`;
+
+
+
+    } else {
+
+        alert("Pregunta desde Open Trivia DB");
+
+    }
+
+
+
+
+
+
+
 });
+
+
+
 
