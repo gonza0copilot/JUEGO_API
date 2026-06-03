750a76b75b845fd0c3d3bfc6b1e6d4e4cd558afe
lucas miranda <lmiranda@obralapiedad.com.ar>
2026-06-03 13:07:48 -0300

    commit "ruleta"

--- script.js
+++ script.js
@@ -3,52 +3,41 @@ const botonGirar = document.getElementById('botonGirar');
 
 let gradosTotales = 0;
 
+const categorias = [
+    "GTA IV",
+    "TF2",
+    "L4D2",
+    "MAFIA III",
+    "API",
+];
+
 botonGirar.addEventListener('click', () => {
-    // Generamos un giro aleatorio entre 3000 y 6000 grados
-    const giroAleatorio = Math.floor(Math.random() * 3000) + 3000;
+
+    const giroAleatorio =
+        Math.floor(Math.random() * 3000) + 3000;
+
     gradosTotales += giroAleatorio;
 
-    // Aplicamos la rotación
-    ruleta.style.transform = `rotate(${gradosTotales}deg)`;
+    ruleta.style.transform =
+        `rotate(${gradosTotales}deg)`;
 });
 
-const categoria = "gta4";
-const preguntas = datos[categoria];
-const preguntaAleatoria =
-    preguntas[Math.floor(Math.random() * preguntas.length)
-    ];
+ruleta.addEventListener('transitionend', () => {
 
-const categoria = "tf2";
-const pregunta =
-    preguntas[categoria][
-    Math.floor(Math.random() * preguntas[categoria].length)
-    ];
+    const gradosFinales = gradosTotales % 360;
 
-const categoria = "l4d2";
-const pregunta =
-    preguntas[categoria][
-    Math.floor(Math.random() * preguntas[categoria].length)
-    ];
+    const gradosPorSector = 360 / categorias.length;
 
-const categoria = "mafia3";
-const pregunta =
-    preguntas[categoria][
-    Math.floor(Math.random() * preguntas[categoria].length)
-    ];
+    const indice =
+        Math.floor(
+            ((360 - gradosFinales) % 360)
+            / gradosPorSector
+        );
 
-fetch(
-  "https://opentdb.com/api.php?amount=1&category=15&difficulty=medium&type=multiple"
-)   
-async function obtenerPreguntaAPI() {
-    const respuesta = await fetch(
-        "https://opentdb.com/api.php?amount=1&category=15&difficulty=medium&type=multiple"
-    );
+    const categoria = categorias[indice];
 
-    const datos = await respuesta.json();
+    console.log("Categoría:", categoria);
+    alert("Salió: " + categoria);
 
-    const pregunta = datos.results[0];
+});
 
-    console.log("Pregunta:", pregunta.question);
-    console.log("Correcta:", pregunta.correct_answer);
-    console.log("Incorrectas:", pregunta.incorrect_answers);
-} 
